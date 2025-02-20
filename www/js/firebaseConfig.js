481a046dfe65948c6fd02c0434a16ec4390c354e
O001830 <fernando.aguilar.santos@bbva.com>
2025-02-20 15:36:11 +0100

    first init

---
+++ www/js/firebaseConfig.js
@@ -0,0 +1,18 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// import { enableLogging } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdVEnvo0TOsbgQnZn_u--m70H0zu4754k",
+  authDomain: "mariayantoniowedding.firebaseapp.com",
+  projectId: "mariayantoniowedding",
+  storageBucket: "mariayantoniowedding.firebasestorage.app",
+  messagingSenderId: "684479880419",
+  appId: "1:684479880419:web:e7b8b49aeef1d4cfec79d6",
+  measurementId: "G-YQLQ5NLX7S"
+};
+
+
+
+export const firebaseApp = initializeApp(firebaseConfig);
+// enableLogging(true);
